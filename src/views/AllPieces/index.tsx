247ec36b33633614236624fa4b4eee117fdd9a17
sokoljacob <103051743+sokoljacob@users.noreply.github.com>
2022-12-17 23:37:27 -0600

    a

--- src/views/AllPieces/index.tsx
+++ src/views/AllPieces/index.tsx
@@ -4,24 +4,17 @@ import styles from "./index.module.css";
 
 export const AllPieces: FC = ({}) => {
   return (
-    <div className="join-content">
-      	<div className="left-content">
-			<h1 className="mb-5 text-3xl font-bold">How do I join?</h1>
-			<div className="indent font-bold">
-				New Realms are released each week until the 118 piece collection is complete. Auctions take place on <Link href="https://exchange.art/collections/Realms"><a target="_blank">exchange.art</a></Link>.
-				<br /><br />
-				In addition, Realms may occasionally be listed for secondary sale on <Link href="https://exchange.art/collections/Realms"><a target="_blank">exchange.art</a></Link> or you can make an offer to an existing Realms holder. Keep your eyes peeled!
-				<br /><br />
-				Follow us on Twitter - <Link href="https://twitter.com/TheRealmsDAO"><a target="_blank">@TheRealmsDao</a></Link> - to stay up to date.
-				<br /><br />
-				<Link href="https://discord.gg/9v3pAw6J"><a target="_blank">Join our Discord</a></Link> if you have any questions or want to network with the Realms holders. We won't bite!
-			</div>
-			<br /><br />
-		</div>
-		<div className="right-join-image">
-			<img id="joinwhiteimg" src="/joinbg-white.png"></img>
-			<img id="joindarkimg" src="/joinbg-dark.png"></img>
-      	</div>
+    <div className="text-center">
+      <a href="https://images-cdn.exchange.art/5PyfVJ40oKaNkKG3vpNN_AOYasMMn5APjzw_Y8oIwSI?ext=fastly&auto=avifwebp" target="_blank">'San Ignacio' by ebro</a><br />
+	  <a href="https://images-cdn.exchange.art/4Xe5zDGU6GZCyprJSSTbXW3GMEATrOEopsQhsK29WqY?ext=fastly&auto=avifwebp" target="_blank">'bricks' by ebro</a><br />
+	  <a href="https://formfunction.imgix.net/nft-images/lpFu6-M9xYFJeCgflb-tp.jpg?q=100&auto=format&ixlib=react-9.5.4&w=2257" target="_blank">'I Want The World To Be Fair' by Emila Yusof</a><br />
+	  <a href="https://formfunction.imgix.net/nft-images/g1bGkAYEL2AgiExf8RhHG.jpg?q=100&auto=format&ixlib=react-9.5.4&w=2257" target="_blank">'Paraty-Contrasts#3' by Art Pardini</a><br />
+	  <a href="https://images-cdn.exchange.art/fEfOalZezHK0QYUSIMFPBqnfip_k8TWQms2WY9KpujU?ext=fastly&auto=avifwebp" target="_blank">'Blocks' by BlockFramez</a><br />
+	  <a href="https://formfunction.imgix.net/nft-images/4Ep6Q5SdeoBz-XiX0uvor.jpg?q=100&auto=format&ixlib=react-9.5.4&w=2257" target="_blank">'pink walls' by Pigment</a><br />
+	  <a href="https://formfunction.imgix.net/nft-images/790O6kL3Fp6FvwIcP4amI.jpg?q=100&auto=format&ixlib=react-9.5.4&w=2257" target="_blank">'Luminary 03' by Pigment</a><br />
+	  <a href="https://formfunction.imgix.net/nft-images/dX1e_by162k7kdLQcNPAa.jpg?q=100&auto=format&ixlib=react-9.5.4&w=2257" target="_blank">'Abstract' by rishehri</a><br />
+	  <a href="https://formfunction.imgix.net/nft-images/01TLCrrhnQogZkTMIOQQx.jpg?q=100&auto=format&ixlib=react-9.5.4&w=2257" target="_blank">'#DF68' by Ben Voxman</a><br />
+	  <a href="https://images-cdn.exchange.art/3gaOrJl_EV5S5JLgw4ac95G_4VUod-DRcIdh3NIEZ-I?ext=fastly&auto=avifwebp" target="_blank">'Whiskey sour' by Hanna BY</a><br />
     </div>
   );
 };
